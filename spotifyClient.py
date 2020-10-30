ba932fe77d94186cf1e5ee75718fe14456848f3e
wjwd <wjwd13@gmail.com>
2020-10-29 21:10:42 -0400

    Added spotifyClient class and helper functions

--- spotifyClient.py
+++ spotifyClient.py
@@ -4,11 +4,28 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import os
 import json
 
+#Eventually move these global variables into threaded_client and pass them into constructor
 SCOPE = "user-read-playback-state user-modify-playback-state"
-spAuth = spotipy.SpotifyPKCE(client_id="cc9859af7bda4fea8c2e74321becb949",redirect_uri="http://localhost:8888/callback",scope=SCOPE,open_browser=True)
+CLIENT_ID = "cc9859af7bda4fea8c2e74321becb949"
+REDIRECT_URI = "http://localhost:8888/callback"
 
-authCode = spAuth.get_authorization_code()
+class spotifyClient:
 
-accToken = spAuth.get_access_token(code=authCode,check_cache=False)
+    def __init__(self, clientID, redirect_uri, scope):
+        self.clientID = CLIENT_ID
+        self.redirect_uri = REDIRECT_URI
+        self.scope = SCOPE
+        self.spAuth = None
+        self.accToken = None
 
-print(accToken)
+
+    def authorizationSetup(self):
+        self.spAuth = spotipy.SpotifyPKCE(self.clientID, self.redirect_uri, self.scope, open_browser=True)
+        self.accToken = self.spAuth.get_access_token(code=self.spAuth.get_authorization_code(),check_cache=False)
+        
+    def getAuthCode(self):
+        return self.spAuth.get_authorization_code()
+
+    def getAccToken(self, authCode):
+        return self.spAuth.get_access_token(code=authCode, check_cache=False)
+
