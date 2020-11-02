5479630a245e2aa2ea94aa8ae4bcf11afc61f615
wjwd <wjwd13@gmail.com>
2020-11-02 16:57:54 -0500

    Changed access token string to full token json

--- threaded_server.py
+++ threaded_server.py
@@ -58,7 +58,8 @@ class Server:
                 elif message["HEADER"] == "CREATE":
                     session_id = "".join(random.choices(string.ascii_uppercase + string.digits, k = 4))
                     indentifer = json.loads(message["MESSAGE"])
-                    self.create_session(session_id, message["ID"], indentifer["display_name"], indentifer["spotify_token"])
+                    tokenDict = json.loads(indentifer["spotify_token"])
+                    self.create_session(session_id, message["ID"], indentifer["display_name"], tokenDict)
                     self.add_connection_entry(message["ID"], indentifer["display_name"], session_id, True, conn, addr)
                     self.create_spotify_player(session_id)
                     client_id = message["ID"]
@@ -366,7 +367,7 @@ class Server:
 
     def create_spotify_player(self, session_id):
         token = self.sessions[session_id]["HOST"]["spotify_token"]
-        self.sessions[session_id]["HOST"]["spotify_player"] = spotifyServer(accToken=token, accTokenDict=None)
+        self.sessions[session_id]["HOST"]["spotify_player"] = spotifyServer(accToken=None, accTokenDict=token)
 
     #-----------------------------END HELPER FUNCTIONS-----------------------------#
 
